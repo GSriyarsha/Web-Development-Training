77f49657aba63290fdae5abe9769c6a44e07ec9f
GSriyarsha <pgsriyarsha@gmail.com>
2020-04-30 23:15:57 +0530

    assignment 1

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Assignment1Component } from './assignment1/assignment1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Assignment1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
